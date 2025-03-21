8c77709a6ce962bb47ef2a0cee7cb0409383032c
anap-arejano <aparejano@gmail.com>
2025-03-21 12:43:20 -0300

    :blue_book: Branch!

--- lerolero.py
+++ lerolero.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
+""" https://github.com/anap-arejano/LeroLero.git """
+
 """Gerador de lero-lero
- 
  Gera frases de efeito sem significado real."""
  
  import random
- 
- parte1 = []
- parte2 = []
- parte3 = []
+
+     parte1 = ["O sistema em desenvolvimento" , "O novo protocolo de comunicação", "O algoritmo foi otimizado e"]
+     parte2 = ["possui excelente desempenho", "oferece garantias de precisão acima da média", "preenche uma lacuna significativa"]
+     parte3 = ["nas aplicações a que se destina", "em relação às opções disponível no mercado" , ", provendo ampla vantagem competitiva a seus usuários"]
 
  lingua = int(input("Escolha a língua: 1 - português; 2 - inglês\n "))
  
  if lingua == 2:
-     parte1 = ["O sistema em desenvolvimento" , "O novo protocolo de comunicação", "O algoritmo foi otimizado e"]
-     parte2 = ["possui excelente desempenho", "oferece garantias de precisão acima da média", "preenche uma lacuna significativa"]
-     parte3 = ["nas aplicações a que se destina", "em relação às opções disponível no mercado" , ", provendo ampla vantagem competitiva a seus usuários"]
+     parte1 = []
+     parte2 = []
+     parte3 = []
  
  print (random.choice(parte1), random.choice(parte2), random.choice(parte3))
