f1bfa73ed05477dd381a4adb64ad1694ba24d7a7
anap-arejano <aparejano@gmail.com>
2025-03-21 12:17:11 -0300

    :sparkles:

--- lerolero.py
+++ lerolero.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Gerador de lero-lero
+ 
+ Gera frases de efeito sem significado real."""
+ 
+ import random
+ 
+ parte1 = []
+ parte2 = []
+ parte3 = []
+ 
+ print (random.choice(parte1), random.choice(parte2), random.choice(parte3))
